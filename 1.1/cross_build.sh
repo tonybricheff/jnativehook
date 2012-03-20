24907574b5bcee135584d35a565aa1e46e2da979
Alexander Barker <alex@1stleg.com>
2012-03-20 04:45:41 +0000

    Updating cross compiler property files.

--- 1.1/cross_build.sh
+++ 1.1/cross_build.sh
@@ -2,9 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+ant deep-clean compile-java
+
 for file in ${DIR}/build.*.properties
 do
-	ant -propertyfile $file clean compile || exit $?
+	rm -Rvf ./bin/obj/ ./build.properties
+	ant -propertyfile $file compile-native strip-native || exit $?
 done
 
 ant package
