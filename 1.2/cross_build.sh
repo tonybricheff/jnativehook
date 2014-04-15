1c67175effbe34334f7896f7930da861514e3302
Alexander Barker <alex@1stleg.com>
2014-04-15 00:23:15 -0700

    Changed from i686 to i586. Fixed cross build script.

--- 1.2/cross_build.sh
+++ 1.2/cross_build.sh
@@ -4,15 +4,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
 
-git clone --depth=1 https://github.com/kwhat/jnativehook
+if [ ! -d jnativehook ]; then
+	git clone --depth=1 https://github.com/kwhat/jnativehook
+fi
+
 cd jnativehook
+
+ant distclean download-jnitasks download-libuiohook
+
+# Fix github clock skew
+find ./src/libuiohook -exec touch {} \;
+find ./src/jnitasks -exec touch {} \;
 
 ant compile-jnitasks compile-java bootstrap-libuiohook
 
 for file in ${DIR}/build.*.properties
 do
-	rm -Rvf ./bin/libuiohook ./bin/jni
-	
+	rm -Rvf ./bin/libuiohook ./bin/obj ./bin/lib
 	ant -propertyfile $file compile-libuiohook compile-jni || exit $?
 done
 
